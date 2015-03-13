c48e58a07a0180d1ad355dd05150f39af1954575
GaraninMS <GaraninMS@samnipineft.ru>
2015-03-13 14:09:08 +0400

    Переименование проекта ReplaceBackgroundWorker

---
+++ ReplaceBackgroundWorker.Tests/CalculatorTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using Moq;
+using NUnit.Framework;
+
+namespace ReplaceBackgroundWorker.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void TestCalculateMethod()
+        {
+            var asyncManagerMock = new Mock<IAsyncManager>();
+
+            asyncManagerMock
+                .Setup(
+                    pa => pa.BackgroundTask(
+                        It.IsAny<Action<DoWorkEventArgs>>(),
+                        It.IsAny<Action<RunWorkerCompletedEventArgs>>(),
+                        It.IsAny<Action<Exception>>()))
+                .Callback<Action<DoWorkEventArgs>, Action<RunWorkerCompletedEventArgs>, Action<Exception>>(
+                    (action, onCompleted, error) =>
+                    {
+                        if (action == null) return;
+
+                        var workArgs = new DoWorkEventArgs(null);
+
+                        try
+                        {
+                            action(workArgs);
+                        }
+                        catch (Exception ex)
+                        {
+                            error(ex);
+                        }
+
+                        if (onCompleted != null)
+                            onCompleted(new RunWorkerCompletedEventArgs(workArgs.Result, null, false));
+                    });
+
+            var calculator = new Calculator(asyncManagerMock.Object);
+            calculator.Calculate();
+        }
+    }
+}
---
+++ ReplaceBackgroundWorker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReplaceBackgroundWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new MyAsyncAwaitTest();
+            test.Run();
+
+            Console.WriteLine("А программа выполняется...");
+            Console.ReadLine();
+        }
+    }
+}
