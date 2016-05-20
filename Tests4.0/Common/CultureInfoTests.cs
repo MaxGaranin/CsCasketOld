3372907427e7a8f1a4336f59fa5f571668c8d775
GaraninMS <GaraninMS@samnipineft.ru>
2016-05-20 08:34:23 +0400

    Тесты для CultureInfo

---
+++ Tests4.0/Common/CultureInfoTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Test40.Common
+{
+    [TestFixture]
+    public class CultureInfoTests
+    {
+        [Test]
+        public void Test()
+        {
+            var culture = Thread.CurrentThread.CurrentCulture;    // Формат из панели управления
+            var uiCulture = Thread.CurrentThread.CurrentUICulture;
+
+            Console.WriteLine("Culture: {0}", culture);
+            Console.WriteLine("UI Culture: {0}", uiCulture);
+        }
+    }
+}
