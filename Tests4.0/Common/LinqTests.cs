82c06978ba3bfc9d0b55e274e834ef53cb37b388
GaraninMS <GaraninMS@samnipineft.ru>
2016-08-23 09:07:32 +0400

    LinqTests

---
+++ Tests4.0/Common/LinqTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Test40.Common
+{
+    [TestFixture]
+    public class LinqTests
+    {
+        [Test]
+        public void Test()
+        {
+            IEnumerable<string> collection = new List<string>();
+            Assert.IsTrue(collection.All(string.IsNullOrEmpty));
+            Assert.IsFalse(collection.Any());
+        } 
+    }
+}
