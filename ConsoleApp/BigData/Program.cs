1559e9db3b3848b29b9042c3ec019f146053d152
GaraninMS <GaraninMS@samnipineft.ru>
2016-10-27 13:20:25 +0400

    Убрал Expressions

--- ConsoleApp/BigData/Program.cs
+++ ConsoleApp/BigData/Program.cs
@@ -7,8 +7,8 @@ namespace ConsoleApp.BigData
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Size of GUID: {0}", Marshal.SizeOf(typeof(Guid)));
-            Console.WriteLine("Size of int: {0}", sizeof(int));
+            Console.WriteLine("Size of GUID: {0}", Marshal.SizeOf(typeof (Guid)));
+            Console.WriteLine("Size of int: {0}", sizeof (int));
 
             int count = 1000000;
             var array = new MyDataItem[count];
@@ -19,18 +19,13 @@ namespace ConsoleApp.BigData
             }
             GC.Collect();
 
-            var msg = $"Memory: {GC.GetTotalMemory(true).ToString("0,0")}";
+            var msg = String.Format("Memory: {0}", GC.GetTotalMemory(true).ToString("0,0"));
             Console.WriteLine(msg);
             Console.ReadKey();
         }
 
         public struct MyDataItem
         {
-//            public MyDataItem()
-//            {
-//                Id = Guid.NewGuid();
-//            }
-
             public Guid Id { get; set; }
             public int Value1 { get; set; }
             public int Value2 { get; set; }
