8633c0059fc7d3e735cb3f41dea1cef97b78d99e
GaraninMS <GaraninMS@samnipineft.ru>
2016-10-28 11:09:33 +0400

    Правки в тесте больших массивов

--- ConsoleApp/BigData/Program.cs
+++ ConsoleApp/BigData/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp.BigData
             Console.WriteLine("Size of GUID: {0}", Marshal.SizeOf(typeof (Guid)));
             Console.WriteLine("Size of int: {0}", sizeof (int));
 
-            int count = 1000000;
+            int count = 80000000;
             var array = new MyDataItem[count];
 
             for (int i = 0; i < array.Length; i++)
@@ -19,12 +19,14 @@ namespace ConsoleApp.BigData
             }
             GC.Collect();
 
-            var msg = String.Format("Memory: {0}", GC.GetTotalMemory(true).ToString("0,0"));
+            var msg = String.Format("Memory: {0} bytes", GC.GetTotalMemory(true).ToString("0,0"));
             Console.WriteLine(msg);
             Console.ReadKey();
         }
 
+        // Занимает намного меньше места
         public struct MyDataItem
+        // public class MyDataItem
         {
             public Guid Id { get; set; }
             public int Value1 { get; set; }
