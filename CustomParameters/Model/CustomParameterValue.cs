29ce7b3b0c47b6229ceb89bb45971b4a95e0c9bc
GaraninMS <maxgaranin@gmail.com>
2016-11-30 20:04:07 +0400

    Добавил проект Parameters

---
+++ CustomParameters/Model/CustomParameterValue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CustomParameters.Model
+{
+    public abstract class CustomParameterValue
+    {
+        public DateTime Date { get; set; }
+
+        public abstract object ObjectValue { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+
+    public class CustomParameterValue<TValue> : CustomParameterValue
+    {
+        public TValue Value { get; set; }
+
+        public override object ObjectValue {
+            get { return Value; }
+            set { Value = (TValue) value; }
+        }
+    }
+
+
+}
