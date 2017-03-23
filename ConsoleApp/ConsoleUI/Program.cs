8724ec9a7bb6ffb469d14e0899f84aeecb40472c
GaraninMS <GaraninMS@samnipineft.ru>
2017-03-23 08:30:27 +0400

    ConsoleUI

---
+++ ConsoleApp/ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp.ConsoleUI
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(10, 10);
+
+                Console.WriteLine("Прогресс выполнения {0}", i);                
+                Thread.Sleep(100);
+            }
+            
+            Console.ResetColor();
+            Console.ReadLine();
+        }
+    }
+}
