a069c83a245f04b7d0f52c07de35dc433d2cea98
GaraninMS <GaraninMS@samnipineft.ru>
2017-06-21 11:56:29 +0400

    Интересный момент при наследовании

---
+++ Tests/Common/ImplicitInheritance.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Tests45.Common
+{
+    public interface IElementParametersRepository
+    {
+        void BatchDelete(IEnumerable ids);
+    }
+
+    public class BaseLiteDbRepository
+    {
+        public void BatchDelete(IEnumerable ids)
+        {
+        }
+    }
+
+    public class ElementParametersRepository : BaseLiteDbRepository, IElementParametersRepository
+    {
+        
+    }
+
+    public class ElementParameters
+    {
+        public int Id { get; set; }
+
+        public double Value { get; set; }
+    }
+
+    public class Work
+    {
+        private readonly IElementParametersRepository _elementParametersRepository;
+
+        public Work(IElementParametersRepository elementParametersRepository)
+        {
+            _elementParametersRepository = elementParametersRepository;
+        }
+
+        public void Run()
+        {
+            _elementParametersRepository.BatchDelete(new List<int>());
+        }
+    }
+}
