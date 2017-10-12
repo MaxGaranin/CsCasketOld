fbf89116fce388e8b2abc0db761368a1d4cf03a3
GaraninMS <GaraninMS@samnipineft.ru>
2017-10-12 14:53:52 +0400

    Изучение параллельности вычислений

---
+++ ConsoleApp/ParallelTasks/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.ParallelTasks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var names = new List<string> {"Мама", "мыла", "раму", "Завтра", "новый", "день", "zuzu", "abarvalg"};
+
+            var counter = 0;
+            Parallel.ForEach(names,
+                (name, state) =>
+                {
+                    Interlocked.Exchange(ref counter, counter + 1);
+                    Console.WriteLine("Step {0} of {1}", counter, names.Count);
+                });
+
+            Console.ReadKey();
+        }
+    }
+}
