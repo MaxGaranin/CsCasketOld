5166013bde0b9ad3028b30a2b7a663c08012ad61
GaraninMS <GaraninMS@samnipineft.ru>
2017-12-07 11:39:55 +0400

    Код для самоудаления программы

---
+++ ConsoleApp/SelfDelete/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace ConsoleApp.SelfDelete
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var info = new ProcessStartInfo();
+            info.Arguments = "/C choice /C Y /N /D Y /T 1 & del " + Assembly.GetExecutingAssembly().Location;
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+            info.CreateNoWindow = true;
+            info.FileName = "cmd.exe";
+            Process.Start(info);
+        }
+    }
+}
