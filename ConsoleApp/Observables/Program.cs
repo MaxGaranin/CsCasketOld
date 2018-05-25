d9a6bffbdcbbbf6c24c058d33547cb5695f3820e
GaraninMS <GaraninMS@samnipineft.ru>
2018-05-25 11:56:34 +0400

    Знакомство с Observables

---
+++ ConsoleApp/Observables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace ConsoleApp.Observables
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IObservable<int> range =
+                from i in Observable.Range(1, 10)
+                where i % 2 == 0
+                select i;
+
+            range.Subscribe(
+                i => Console.WriteLine("Next element: {0}", i),
+                e => Console.WriteLine("Error: {0}", e.Message),
+                () => Console.WriteLine("Range observation complete")
+            );
+
+            Console.ReadLine();
+        }
+    }
+}
--- WpfSamples/View/ValidationTestView.xaml.cs
+++ WpfSamples/View/ValidationTestView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Reactive.Linq;
+using System.Windows;
+using System.Windows.Input;
 
 namespace WpfSamples40.View
 {
@@ -7,6 +10,23 @@ namespace WpfSamples40.View
         public ValidationTestView()
         {
             InitializeComponent();
+
+            TestObservables();
+        }
+
+        private void TestObservables()
+        {
+            var eventsAsObservable = (
+                    from move in Observable.FromEventPattern<MouseButtonEventArgs>(this, "MouseDown")
+                    let pos = move.EventArgs.GetPosition(this)
+                    select new { pos.X, pos.Y }
+                )
+                .TimeInterval()
+                .Where(e => e.Interval.TotalMilliseconds < 500);
+
+            eventsAsObservable.Subscribe(e =>
+                Console.WriteLine(@"Double click: X={0}, Y={1}, Interval={2}",
+                    e.Value.X, e.Value.Y, e.Interval));
         }
     }
 }
