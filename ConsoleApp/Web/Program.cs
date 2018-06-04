74cae0074eeb4eeea6294b748a80a8a29362e99a
GaraninMS <GaraninMS@samnipineft.ru>
2018-06-04 11:28:17 +0400

    Async in console

---
+++ ConsoleApp/Web/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Web
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            MainAsync().GetAwaiter().GetResult();
+            // MainAsync().Wait();  // а можно и так
+
+            Console.ReadKey();
+        }
+
+        private static async Task MainAsync()
+        {
+            Console.WriteLine("----- Enter main");
+            await TestHttpClient();
+            Console.WriteLine("----- Exit main");
+        }
+
+        private static async Task TestHttpClient()
+        {
+            var webProxy = WebRequest.GetSystemWebProxy();
+            webProxy.Credentials = new NetworkCredential(@"samnipineft\Garanin_MS", "iSochi06");
+            WebRequest.DefaultWebProxy = webProxy;
+
+            var url = "https://www.sports.ru/rss/main.xml";
+
+            using (var client = new HttpClient(new HttpClientHandler() {Proxy = WebRequest.DefaultWebProxy}))
+            {
+                using (var response = await client.GetAsync(url))
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
