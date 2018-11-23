1b0c131e86012f347390bc02e1b4dcd1d4d7e70e
GaraninMS <GaraninMS@samnipineft.ru>
2018-11-23 16:06:09 +0400

    Добавил примеры работы с примитивами синхронизации из книги Албахари

---
+++ ConsoleApp/Async/Albahari/AcknowledgedWaitHandle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp.Async.Albahari
+{
+    public class AcknowledgedWaitHandle
+    {
+        static EventWaitHandle ready = new AutoResetEvent(false);
+        static EventWaitHandle go = new AutoResetEvent(false);
+        static volatile string task;
+ 
+        static void Main() 
+        {
+            new Thread(Work).Start();
+ 
+            // Сигнализируем рабочему потоку 5 раз
+            for (int i = 1; i <= 5; i++) 
+            {
+                ready.WaitOne();             // Сначала ждем, когда рабочий поток будет готов
+                task = "a".PadRight(i, 'h'); // Назначаем задачу
+                go.Set();                    // Говорим рабочему потоку, что можно начинать
+            }
+ 
+            // Сообщаем о необходимости завершения рабочего потока,
+            // используя null-строку
+            ready.WaitOne();
+            task = null;
+            go.Set();
+
+            Console.ReadKey();
+        }
+ 
+        static void Work() 
+        {
+            while (true) 
+            {
+                ready.Set();  // Сообщаем о готовности
+                go.WaitOne(); // Ожидаем сигнала начать...
+
+                if (task == null)
+                    return; // Элегантно завершаемся
+
+                Console.WriteLine(task);
+            }
+        }
+    }
+}
