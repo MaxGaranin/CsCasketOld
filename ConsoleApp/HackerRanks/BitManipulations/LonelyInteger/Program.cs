9c292a5cf082a3c08bce557679b7576118b8226d
GaraninMS <GaraninMS@samnipineft.ru>
2018-12-21 15:52:04 +0400

    Пока решение задачи ThreeSum не проходит по тайм-ауту

---
+++ ConsoleApp/HackerRanks/BitManipulations/LonelyInteger/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp.HackerRanks.BitManipulations.LonelyInteger
+{
+    public class Program
+    {
+        private static int Lonelyinteger(int[] a)
+        {
+            var hash = new HashSet<int>();
+            for (var i = 0; i < a.Length; i++)
+            {
+                if (hash.Contains(a[i]))
+                {
+                    hash.Remove(a[i]);
+                }
+                else
+                {
+                    hash.Add(a[i]);
+                }
+            }
+
+            return hash.Single();
+        }
+
+        public static void Main()
+        {
+            var a = new int[] {4, 9, 95, 93, 57, 4, 57, 93, 9};
+            var result = Lonelyinteger(a);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp/LeetCodes/ThreeSum/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp.LeetCodes.ThreeSum
+{
+    public class Program
+    {
+        public static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            var results = new List<IList<int>>();
+
+            for (var i = 0; i < nums.Length - 2; i++)
+            {
+                for (var j = i + 1; j < nums.Length - 1; j++)
+                {
+                    for (var k = j + 1; k < nums.Length; k++)
+                    {
+                        var sum = nums[i] + nums[j] + nums[k];
+                        if (sum == 0)
+                        {
+                            var test = new List<int>
+                            {
+                                nums[i], nums[j], nums[k]
+                            };
+                            test.Sort();
+
+                            var isDuplicate = false;
+                            foreach (var result in results)
+                            {
+                                if (result.SequenceEqual(test))
+                                {
+                                    isDuplicate = true;
+                                    break;
+                                }
+                            }
+
+                            if (isDuplicate) break;
+
+                            var newResult = new List<int>
+                            {
+                                nums[i], nums[j], nums[k]
+                            };
+                            newResult.Sort();
+
+                            results.Add(newResult);
+                        }
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        public static void Main()
+        {
+            var sr = new StreamReader(@".\..\..\LeetCodes\ThreeSum\input01.txt");
+            var s = sr.ReadLine();
+            var nums = s.Split(',').Select(int.Parse).ToArray();
+            sr.Close();
+
+            var result = ThreeSum(nums);
+            foreach (var item in result)
+            {
+                var str = string.Join(",", item);
+                Console.WriteLine(str);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
