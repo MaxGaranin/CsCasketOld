3cea455ea0de2c2ae00322446abe6c498cc86271
GaraninMS <GaraninMS@samnipineft.ru>
2019-02-08 13:24:36 +0400

    Доделал задачу на сихронизацию потоков (из книги Гослинга)

--- ConsoleApp/Async/ThreadsSynchronization/Gosling.cs
+++ ConsoleApp/Async/ThreadsSynchronization/Gosling.cs
@@ -4,15 +4,21 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp.Async.ThreadsSynchronization
 {
+    /// <summary>
+    /// Задача на многопоточность из книги Гослинга
+    /// https://ru.stackoverflow.com/questions/338205/Задача-на-многопоточность-из-книги-Гослинга
+    /// </summary>
     public class Gosling
     {
         private static readonly object Lock = new object();
         private static int _counter = 0;
+        private static int _end = 15;
 
         public static void Main(string[] args)
         {
-            var waiterTask = Task.Run(() => Waiter());
-            var timerTask = Task.Run(() => Worker());
+            var waiter5Task = Task.Run(() => Waiter(5));
+            var waiter7Task = Task.Run(() => Waiter(7));
+            var workerTask = Task.Run(() => Worker());
 
             Console.ReadKey();
         }
@@ -22,32 +28,30 @@ namespace ConsoleApp.Async.ThreadsSynchronization
             while (true)
             {
                 Thread.Sleep(1000);
-                Console.WriteLine($"Прошло секунд: {_counter}");
                 _counter++;
+                Console.WriteLine($"Прошло секунд: {_counter}");
 
                 lock (Lock)
                 {
                     Monitor.PulseAll(Lock);
                 }
 
-                if (_counter > 100) break;
+                if (_counter >= _end) break;
             }
         }
 
-        internal static void Waiter()
+        internal static void Waiter(int n)
         {
             lock (Lock)
             {
                 while (true)
                 {
-                    if (_counter > 0 && _counter % 5 != 0)
+                    if (_counter > 0 && _counter % n == 0)
                     {
                         Console.WriteLine("Получили сигнал!");
                     }
-                    else
-                    {
-                        Monitor.Wait(Lock);
-                    }
+
+                    Monitor.Wait(Lock);
                 }
             }
         }
