4bfe3034c8df94da8156d28d8b4974f6651c10ec
GaraninMS <GaraninMS@samnipineft.ru>
2019-04-10 10:02:03 +0400

    Изучение наследования от обычного контрола и переопределение шаблона отображения

---
+++ WpfControls/MyTextBox.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfControls
+{
+    public class MyTextBox : TextBox
+    {
+        static MyTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyTextBox), new FrameworkPropertyMetadata(typeof(MyTextBox)));
+        }
+    }
+}
--- WpfSamples/View/NotifyIconView.xaml.cs
+++ WpfSamples/View/NotifyIconView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Markup;
+using System.Xml;
 
 namespace WpfSamples.View
 {
@@ -12,6 +16,21 @@ namespace WpfSamples.View
         private void CloseButton_OnClick(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void NotifyIconView_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            XmlWriterSettings xmlSettings = new XmlWriterSettings();
+            xmlSettings.Indent = true;
+
+            using (XmlWriter xmlWriter = XmlWriter.Create(stringBuilder, xmlSettings))
+            {
+                XamlWriter.Save(TextBox.Template, xmlWriter);
+            }
+
+            Console.WriteLine(stringBuilder.ToString());
         }
     }
 }
