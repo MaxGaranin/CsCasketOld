8cf55c13b10f1ffcac545926c89f6a532dc71ed5
GaraninMS <GaraninMS@samnipineft.ru>
2019-07-05 09:00:31 +0400

    Работа над задачами HackerRank

---
+++ ConsoleApp/HackerRanks/Dictionaries/SherlockAndAnagrams/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp.HackerRanks.Dictionaries.SherlockAndAnagrams
+{
+    public class Program
+    {
+        private static int SherlockAndAnagrams(string s)
+        {
+            return 0;
+        }
+
+        public static void Main()
+        {
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp/HackerRanks/WarmUp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp.HackerRanks.WarmUp
+{
+    public class Program
+    {
+        static string TwoStrings(string s1, string s2)
+        {
+            var hashSet1 = new HashSet<char>(s1);
+            var hashSet2 = new HashSet<char>(s2);
+            hashSet1.IntersectWith(hashSet2);
+
+            if (hashSet1.Count > 0)
+            {
+                return "YES";
+            }
+            else
+            {
+                return "NO";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var s1 = "Hello";
+            var s2 = "world";
+
+            var result = TwoStrings(s1, s2);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
