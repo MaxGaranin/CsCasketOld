89bf47da599d7389c721f4ff792e55a8f7c70206
GaraninMS <GaraninMS@samnipineft.ru>
2019-09-04 10:31:24 +0400

    Добавил LockByThis

---
+++ ConsoleApp/Async/SomeTasks/LockByThis.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Async.SomeTasks
+{
+    public class LockByThisRunner
+    {
+        public static void Main(string[] args)
+        {
+            var obj = new LockByThis();
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    lock (obj)             
+                    {
+                        Console.WriteLine("Lock");
+                        Thread.Sleep(3000);
+                    }
+                }
+            });
+
+            obj.Go(1);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class LockByThis
+    {
+        public void Go(int k)
+        {
+            while (true)
+            {
+                lock (this)
+                {
+                    Console.WriteLine($"Thread: {k}");
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
